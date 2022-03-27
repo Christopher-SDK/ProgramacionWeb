75acad3e8e634817ce003e1ea25c7097a06fdd09
Christopher Arias <christopherarias2@gmail.com>
2022-03-27 16:49:49 -0500

    Creación de una clase y objeto Student

---
+++ ProgramaciónWeb/semana 1/src/main/java/pe/edu/upc/Program.java
@@ -0,0 +1,14 @@
+package pe.edu.upc;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Student student=new Student();
+		
+		student.setFirstName("Christopher Arias Peña");
+		
+		System.out.println(student.getFirstName());
+	}
+
+}
